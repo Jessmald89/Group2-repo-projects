48106c3daddf2ef3b3484f899ffa997e15e07f78
Bryan Ramsey <116605285+bryancramsey@users.noreply.github.com>
2023-10-10 15:59:59 -0400

    Added some comments for clarity

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from cart import Cart
 from food import Food
 from constant_values import BANANA_PRICE, APPLE_PRICE, ORANGE_PRICE, DISCOUNT, DISCOUNT_CODE
 
-app = Flask(__name__)
+app = Flask(__name__)   # Instantiates the Flask app
 user_cart = Cart()
 
-@app.route("/", methods=["GET", "POST"])
+'''
+@app.route("/", methods=["GET", "POST"]) is a python decorator used by Flask to associate a function with a specific URL, which in this case is simply "/". 
+This just tells Flask that the function "def index()"" should be executed whenever a user accesses the root url of the app ("/"). 
+It also specifies "GET" and "POST" as methods that the route can handle.
+'''
+@app.route("/", methods=["GET", "POST"])  
 def index():
-    if request.method == "POST":
-        add_operation = request.form["add_operation"]
+    if request.method == "POST":       # Provides instructions for a form whose method is "POST"
+        add_operation = request.form["add_operation"]   # Finds element in html template where name="add_operation"
 
         if add_operation == "banana":
             banana = Food("Banana", BANANA_PRICE)
@@ -27,7 +32,7 @@ def index():
             # Apply a discount if the provided code matches
             user_cart.apply_discount(DISCOUNT)  # e.x. 20% discount
 
-    return render_template("index.html", inventory=user_cart.items, subtotal=user_cart.subtotal)
+    return render_template("index.html", inventory=user_cart.items, subtotal=user_cart.subtotal) # Rendering templates is how Flask interfaces with html.
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) # Runs app in debug mode, change debug=False for production version (e.g. for the labs most likely?)
--- constant_values.py
+++ constant_values.py
@@ -1,3 +1,4 @@
+# This file contains all constants used in this program
 BANANA_PRICE = 2.00
 APPLE_PRICE = 3.00
 ORANGE_PRICE = 4.00
