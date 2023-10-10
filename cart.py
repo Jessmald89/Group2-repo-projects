013d210ac0ab74c09aa6a351c08f330c6bc97c44
zok! <112133076+Zokibi@users.noreply.github.com>
2023-10-10 11:46:28 -0400

    Fixed an error with 'app.py', updayed 'cart.py'

--- cart.py
+++ cart.py
@@ -2,6 +2,7 @@ class Cart(object):
     def __init__(self):
         self.items = []
         self.subtotal = 0.0
+        self.discount = 0.0 
 
     def add(self, item):
         self.items.append(item.name)
@@ -12,6 +13,16 @@ class Cart(object):
             raise KeyError(str(item) + " not in cart")
         self.items.remove(item.name)
         self.subtotal -= item.price
+    
+    def apply_discount(self, discount_percentage):
+        if 0 <= discount_percentage <= 1:
+            self.discount = discount_percentage
+        else: 
+            raise ValueError("Disocount percentage must be between 0 and 1.")
+    
+    def calculate_total(self):
+        discounted_total = self.subtotal * (1 - self.discount)
+        return round(discounted_total, 2)
 
     def display(self):
         print(f"Your cart: {self.items}")
