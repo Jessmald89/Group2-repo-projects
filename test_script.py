c44a59570023f9e60ea73b1901b35543ecf6ada7
zok! <112133076+Zokibi@users.noreply.github.com>
2023-10-24 18:31:41 -0400

    Added 'test_total_calculation_with_discount' test

--- test_script.py
+++ test_script.py
@@ -15,6 +15,7 @@ def browser():
 
 def test_title_validation(browser):
     """
+    Test Case ID: 0001
     Test that expected title matches the actual title of the page.
     """
     browser.get('http://localhost:5000')  # Access the app
@@ -23,9 +24,35 @@ def test_title_validation(browser):
 
     assert expected_title == actual_title, "Expected title does not match actual title"
 
+def test_total_calculation_with_discount(browser):
+    """
+    Test Case ID: 0002
+    Tests the total calculation with a discount applied.
+    """
+    browser.get('http://localhost:5000')  # Access the app
+
+    # Select and add a Banana to the cart
+    select = Select(browser.find_element(By.NAME, 'add_operation'))
+    select.select_by_visible_text('Banana')
+
+    # Click "Add to Cart" button
+    add_to_cart = browser.find_element(By.NAME, 'form1_submit')
+    add_to_cart.click()
+
+    # Enter a valid discount code (e.g., 'DISCOUNT20')
+    discount = browser.find_element(By.NAME, 'discount_code')
+    discount.clear()
+    discount.send_keys('DISCOUNT20')
+    discount.send_keys(Keys.RETURN)
+
+    # Verify that the total is correctly calculated (after applying the discount)
+    total_text = browser.find_element(By.XPATH, '//p[contains(text(), "Total: $")]').text
+    assert total_text.startswith('Total: $1.6')  
+
 
 def test_select_add(browser):
     """
+    Test Case ID: 0003, 0004
     Test that items are successfully selected and added to cart.
     """
     browser.get('http://localhost:5000')  # Access the app
@@ -51,6 +78,11 @@ def test_select_add(browser):
     assert "Banana" in cart.text, "Item not added to cart."
 
 def test_remove_item(browser):
+    """
+    Test Case ID: 0005
+    Test that items are successfully removed from cart
+    """
+
     # Access the app
     browser.get('http://localhost:5000')
 
@@ -76,6 +108,7 @@ def test_remove_item(browser):
 
 def test_invalid_item_removal(browser):
     """
+    Test Case ID: 0006
     Test that attempts to remove an item not in cart are properly handled.
     """
     # Access the app
@@ -105,10 +138,15 @@ def test_invalid_item_removal(browser):
     cart = WebDriverWait(browser, 10).until(
         EC.presence_of_element_located((By.NAME, 'item_display'))
     )
-    assert "Orange" not in cart.text, "Invalid item added to cart."
+    assert "Orange" not in cart.text, "Attempted to remove item not in cart"
 
 
 def test_input(browser):
+    """
+    Test Case ID: 0007, 0008
+    Test the input of a discount code and submission
+    """
+
     browser.get('http://localhost:5000')  # Access the app
     test_disc_code = 'DISCOUNT20'
 
@@ -120,6 +158,11 @@ def test_input(browser):
     discount.send_keys(Keys.RETURN)
 
 def test_clear_cart(browser):
+    """
+    Test Case ID: 0009
+    Test that cart is cleared when clicking "Purchase"
+    """
+
     # Access the app
     browser.get('http://localhost:5000')
 
