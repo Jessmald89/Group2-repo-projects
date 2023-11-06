2a849567fa25abbd758b77ddc09365d53eab4a9c
zok! <112133076+Zokibi@users.noreply.github.com>
2023-11-06 16:21:10 -0500

    Added Comments For Clarity

--- features/steps/discount_code_steps.py
+++ features/steps/discount_code_steps.py
@@ -1,19 +1,28 @@
+# Import necessary modules
 from behave import given, when, then
 from app import app, user_cart
-from constant_values import DISCOUNT_CODE, DISCOUNT  # Import the discount code and discount percentage
+from constant_values import DISCOUNT_CODE, DISCOUNT
 
+# Define a "given" step - the fruit stand application is open, and the user has items in their cart
 @given('the fruit stand application is open and the user has items in their cart')
 def step_open_fruit_store_and_user_has_items(context):
+    # Create a test client for the application
     context.client = app.test_client()
-    # You may add items to the cart here (similar to what you did in the "app.py" file).
 
+# Define a "when" step - the user applies a discount code
 @when('the user applies discount code "{discount_code}"')
 def step_apply_discount_code(context, discount_code):
+    # Check if the provided discount code matches the predefined discount code
     if discount_code == DISCOUNT_CODE:
+        # Apply the discount to the user's cart
         user_cart.apply_discount(DISCOUNT)
 
+# Define a "then" step - check if the cart total is updated with a 20% discount
 @then('the cart total should be updated with a 20% discount')
 def step_check_discount_applies(context):
+    # Calculate the expected total after applying the discount
     expected_total = user_cart.calculate_total()
+    # Get the actual total from the user's cart
     actual_total = user_cart.total
+    # Assert that the actual total matches the expected total after applying the discount
     assert actual_total == expected_total
--- features/steps/removing_from_cart_steps.py
+++ features/steps/removing_from_cart_steps.py
@@ -1,17 +1,28 @@
+# Import Behave and necessary modules
 from behave import given, when, then
 from cart import Cart
 from food import Food
 
+# Define a "given" step - the user has items in the cart
 @given('the user has items in the cart')
 def step_user_has_items_in_cart(context):
+    # Create a Cart object to represent the user's shopping cart
     context.store = Cart()
-    context.fruit = Food('Banana', 2.00)  
+    
+    # Create a Food object (e.g., a Banana) with a price of $2.00
+    context.fruit = Food('Banana', 2.00)
+    
+    # Add the Food object to the user's shopping cart
     context.store.add(context.fruit)
 
+# Define a "when" step - the user removes an object from the cart
 @when('the user removes an object from the cart')
 def step_user_removes_object_from_cart(context):
+    # Remove the Food object from the user's shopping cart
     context.store.remove(context.fruit)
 
+# Define a "then" step - check if the object will be removed from the cart
 @then('the object will be removed from the cart')
 def step_check_object_removed_from_cart(context):
+    # Assert that the Food object (context.fruit) is not in the list of item names in the cart
     assert context.fruit not in context.store.item_names
